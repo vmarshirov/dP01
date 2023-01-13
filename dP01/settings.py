356c43b70d5bde48902bf83986f1fe82727b08a2
vmarshirov <vmarshirov@outlook.com>
2023-01-13 20:08:47 +0300

    2023-01-13

--- dP01/settings.py
+++ dP01/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'firsrApp.apps.FirsrappConfig',
+    'firstApp.apps.FirstappConfig',
 ]
 
 MIDDLEWARE = [
--- firstApp/apps.py
+++ firstApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FirsrappConfig(AppConfig):
+class FirstappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'firsrApp'
+    name = 'firstApp'
